2d4a2221545c9460267cc4a30814347dd81ccfc5
Abvorobey <abvorobey@gmail.com>
2023-02-14 20:06:58 +0300

    Двойная кратность

---
+++ Double_Multiplicity/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на ввод число
+и проверяет кратно ли оно одновременно 7 и 23
+14 -> нет
+46 -> нет
+161 -> да*/
+Console.Clear();
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+if (a%7 == 0 && a%23 == 0)
+{Console.WriteLine($"{a} -> да");}
+else {Console.WriteLine($"{a} -> нет");}
