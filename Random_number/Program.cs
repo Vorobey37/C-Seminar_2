3c74ff1a953e0e66f4be42cfb65055a8f0b51d81
Abvorobey <abvorobey@gmail.com>
2023-02-14 18:40:05 +0300

    Максимальная цифра случайного числа

---
+++ Random_number/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая выводит случайное число 
+из отрезка [10, 99] и показывает наибольшую цифру числа*/
+
+Console.Clear();
+int a = new Random().Next(10, 100); //Первое число включается, а второе нет! "[)"
+Console.WriteLine(a);
+int b = a/10;
+int c = a%10;
+int max = b;
+if (c>max)
+{max = c;}
+Console.WriteLine(max);
